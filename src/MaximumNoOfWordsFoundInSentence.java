b3db302344dc1c97e05d7e82a435499bebd8f10a
romit <romitpal4@gmail.com>
2023-08-11 16:03:56 +0530

    leetcode

---
+++ src/MaximumNoOfWordsFoundInSentence.java
@@ -0,0 +1,16 @@
+public class MaximumNoOfWordsFoundInSentence {
+
+
+    class Solution {
+        public int mostWordsFound(String[] sentences) {
+            int maxLen = 0;
+
+            for (String currSent : sentences) {
+                int currLen = currSent.split(" ").length;
+                if (maxLen < currLen)
+                    maxLen = currLen;
+            }
+            return maxLen;
+        }
+    }
+}
